bbb81cbdb298ff8eef5f0d7db844ad8d9585eefc
Jose Pablo Aguero <aguerojosepablo2387@gmail.com>
2024-05-28 12:42:39 -0600

    New componente Builder Base

---
+++ components/dashboard/builder.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <template v-for="(item, index) in elements" :key="index">
+        <component :is="item.tag" v-bind="item.attrs">
+          <template v-if="item.children">
+            <Builder :elements="item.children" />
+          </template>
+          <template v-else>{{ item.content }}</template>
+        </component>
+      </template>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Builder',
+    props: {
+      elements: {
+        type: Array,
+        required: true
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  /* Añade tus estilos aquí si es necesario */
+  </style>
+  
